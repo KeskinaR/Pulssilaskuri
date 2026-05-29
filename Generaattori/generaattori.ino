fd72a1abee0caa7140b2cc2a635e4884361a6702
KeskinaR <keskinarkausnilla@gmail.com>
2026-05-29 21:48:02 +0300

    Create generaattori.ino

---
+++ Generaattori/generaattori.ino
@@ -0,0 +1,27 @@
+//TimerOne kirjaston tulee olla asennettu!!
+//Asentamisohjeet lueteltu alla:
+// 1. Avaa ArduinoIDE 
+// 2. Mene kohtaan Library Manager (Vasen sivu, neljän palkin kuvake)
+// 3. Hae TimerOne
+// 4. paina "INSTALL" ylimmässä vaihtoehdossa (älä asenna TimerOne_V2 tai muita versioita)
+
+#include <TimerOne.h>
+
+//määritetyt lähetysportit
+const int outPin1 = 2;
+const int outPin2 = 3;
+
+void setup() {
+  //lähetetään porteista
+  pinMode(outPin1, OUTPUT);
+  pinMode(outPin2, OUTPUT);
+  
+  //konfiguroidaan pulssin suuruus
+  Timer1.initialize(3300);
+  Timer1.pwm(outPin1, 26);
+  Timer1.pwm(outPin2, 13);
+}
+
+void loop() {
+
+}
